1a31e0966a38af14fdf25d8ce99e109041a46ed3
Good-y <Good-y@gmail.com>
2022-09-12 02:53:28 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+*main - print the string in the put function
+*
+* description: using the main function
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+       char c;
+       int i;
+       long li;
+       long long lli;
+       float f;
+
+      printf("size of a char: Xld byte(s)\n",sizeof(c));
+      printf("size of an int:Xld byte(s)\n",sizeof(i));
+      printf("size of a long:Xld byte(s)\n",sizeof(li));
+      printf("size of a long long:Xld byte(s)\n",size of(lli));
+      printf("size of a float:Xld byte(s)\n";sizeof(f));
+
+
+
+
+
+     return(0);
+}
