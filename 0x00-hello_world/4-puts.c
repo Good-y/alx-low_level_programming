45c1c3c1ee9de64e87752024bba8435c2eedd23b
Good-y <Good-y@gmail.com>
2022-09-12 01:54:52 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main -print the string in the put function
+ *
+ * description -using the main functionction
+ *this program prints "programming is like building a multilingual puzzle"
+ *    * Return:0
+ *
+ *     */
+
+int main(void)
+
+{
+
+	    return (0);
+
+}
