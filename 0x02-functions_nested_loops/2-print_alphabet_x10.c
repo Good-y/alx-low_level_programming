c7d1908eb0fe51ab3e3f31ac39d693e6901816c4
Good-y <Good-y@gmail.com>
2022-09-14 09:00:57 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 {
 	char c, i;
 
-	for (i =0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
              
 		for (c = 'a'; c <= 'z'; c++)
@@ -16,4 +16,5 @@ void print_alphabet_x10(void)
 	                _putchar(c);
                 }
                 _putchar('\n');
-        }
+         }
+}	
